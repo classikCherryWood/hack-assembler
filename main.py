9ea479ff10c19bcddc716d1cdee261d80d516ace
classikCherryWood <shih-hao.chan@bayes.city.ac.uk>
2026-06-15 21:21:26 +0100

    feat/args: added A-ins logic and 16-bit converter

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-import sys
+import sys, math
 
 
 def main(name):
     print(f"Hello from {name}")
+
+
+def process_a_ins(cmd):
+    num = cmd.lstrip("@")
+    return num
+
+
+def is_a_ins(cmd):
+    if cmd.startswith("@"):
+        return True
+
+
+def num_to_16_bit_binary(num):
+    res = ""
+    while num >= 1:
+        res = str(num % 2) + res
+        num = math.floor(num / 2)
+
+    res = "0" * (16 - len(res)) + res
+
+    return res
 
 
 class HackAssembler:
@@ -22,4 +43,11 @@ if __name__ == "__main__":
         if clean_line and not clean_line.startswith("//"):
             clean_list.append(clean_line)
 
+    # A-instruction
+    #    for line in clean_list:
+    #        if is_a_ins(line):
+    #            num = int(line.lstrip("@"))
+    #            binary = num_to_16_bit_binary(num)
+    #            with open(f"{filename.rstrip('.asm')}.hack", "w+") as f:
+    #                f.write(binary)
     print(clean_list)
