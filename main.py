cc343ec3052a1598d8fc92f96136457b8d7c5ab4
classikCherryWood <shih-hao.chan@bayes.city.ac.uk>
2026-06-15 17:25:12 +0100

    feat(args): read, open, parse and preliminary cleaning of .asm

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from assembler!")
+import sys
+
+
+def main(name):
+    print(f"Hello from {name}")
+
+
+class HackAssembler:
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    filename = sys.argv[1]
+    with open(filename, "r") as f:
+        code_list = f.readlines()
+
+    clean_list: list[str] = []
+    for line in code_list:
+        # remove leading spaces & trailing \n char
+        clean_line = line.lstrip().rstrip()
+        # remove empty char & comments
+        if clean_line and not clean_line.startswith("//"):
+            clean_list.append(clean_line)
+
+    print(clean_list)
